0a31e5b8d6d542dd38ae171713cff8ad42b56346
Developer <test@example.com>
2025-09-20 13:23:10 -0500

    feat: implementar búsqueda de productos en CustomHeader

--- src/shop/components/CustomHeader.tsx
+++ src/shop/components/CustomHeader.tsx
@@ -1,10 +1,31 @@
 import { Search, ShoppingBag, Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useSearchParams } from "react-router";
 
 const CustomHeader = () => {
   const [cartCount] = useState(3);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const query = searchParams.get("query") || "";
+
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const newSearchParams = new URLSearchParams();
+    if (e.key === "Enter" && inputRef.current) {
+      const query = inputRef.current.value;
+      if (query.trim().length > 0) {
+        newSearchParams.set("query", query);
+      } else {
+        newSearchParams.delete("query");
+      }
+      setSearchParams(newSearchParams);
+    }
+  };
+
   return (
     <header className="sticky top-0 z-50 w-full border-b backdrop-blur bg-slate-50">
       <div className="container mx-auto px-4 lg:px-8">
@@ -53,8 +74,11 @@ const CustomHeader = () => {
               <div className="relative">
                 <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
                 <Input
+                  ref={inputRef}
                   placeholder="Buscar productos..."
-                  className="pl-9 w-64 h-9"
+                  className="pl-9 w-64 h-9 bg-white"
+                  onKeyDown={handleSearch}
+                  defaultValue={query}
                 />
               </div>
             </div>
