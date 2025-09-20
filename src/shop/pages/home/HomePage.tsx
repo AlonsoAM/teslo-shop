b7e67a5e4e03b14e3db4407226e263f72a33475c
Developer <test@example.com>
2025-09-20 11:47:10 -0500

    feat: add button component and integrate Tailwind CSS
    - Added a new Button component using Radix UI and class-variance-authority for styling.
    - Integrated Tailwind CSS into the project with custom styles and animations.
    - Updated package.json to include new dependencies for Tailwind CSS and utility libraries.
    - Enhanced ShopLayout to include an Outlet for nested routing.
    - Modified HomePage to demonstrate the new Button component and typography styles.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to resolve aliases for cleaner imports.

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <h1 className="font-montserrat font-thin">Hola Mundo</h1>
+      <h1 className="font-montserrat font-normal">Hola Mundo</h1>
+      <h1 className="font-montserrat font-bold">Hola Mundo</h1>
+      <Button variant={"outline"}>Hola Mundo</Button>
+    </>
+  );
 };
